75ef01189e5bc0afbdbc9cc4913fdbba9ed401a6
Abhirajrs1 <132868953+Abhirajrs1@users.noreply.github.com>
2024-04-25 17:14:33 +0530

    update methods

--- Js-Basics/methods.js
+++ Js-Basics/methods.js
@@ -29,3 +29,28 @@ arr.splice(2,0,'F') //Add the value from the index position 2
 let z=arr.slice(3,5) //Slice the value from index 3 to before 5
 console.log(z);
 console.log(arr);
+
+
+// Object methods
+
+let obj={
+    name:"Abhiraj",
+    age:25
+}
+let obj2={
+    name:"Alan",
+    age:19
+}
+
+let result=Object.keys(obj) //Used to retrieve the keys of objects
+let values=Object.values(obj) // Used to retrieve the values of objects
+let entries=Object.entries(obj) //Used to retrieve the full entries in an object
+let ab=Object.assign({},obj,obj2) //Used to merge the objects when the keys are different and when keys are same the latest object override the old objects
+Object.freeze(obj) //Used to freeze an object and doesn't make any updations into that object
+obj.name='Aravind'
+console.log(obj);
+
+Object.seal(obj2) //Used to seal an object and only make update the values
+obj2.name="Aravind"
+obj2.place="TVM"
+console.log(obj2);
